30fe4fe51efc858e8f42ca61d5db393ae7752b55
SerialElf <73801294+SerialElf@users.noreply.github.com>
2024-04-03 22:29:54 -0700

    Allow use of variable for reboot timer
    Should work. Will test before next version

--- restart.sh
+++ restart.sh
@@ -1,23 +1,31 @@
 #/bin/bash
+
+if [[ $1 =~ ^[0-9]+$ ]]; then
+   timer = $1
+   echo = "restarting server in $timer minutes"
+else
+   timer = 5
+   echo "no valid number supplied. Restarting server in 5 minutes"
+fi
+
 announce () {
 	screen -S vintagestory_server -p 0 -X stuff "/announce $1^M"
 }
 command () {
 	screen -S vintagestory_server -p 0 -X stuff "$1^M"
 }
+
 echo "This is reboot script, please be patient"
-announce "Server rebooting in 5 minutes please get to safety"
-echo "first message sent nya~"
-sleep 60
-announce "Server reboot in 4 minutes"
-echo "four minute warning sent nya~"
-sleep 60
-announce "Server reboot in 3 minutes"
-echo "three minute warning sent nya~"
-sleep 60
-announce "Server reboot in 2 minutes"
-echo "two minute warning sent nya~"
-sleep 60
+
+while [ $timer -gt 1 ]
+do
+	unset $1
+	announce "server is rebooting in $timer minutes please get to safety"
+	echo "$timer minute warning sent nya~"
+	timer = $(( timer - 1))
+	sleep 60
+done
+
 announce "Server will reboot in 60 seconds, please log off if able. Get to safety otherwise."
 echo "one minute warning sent nya~"
 sleep 50
@@ -29,7 +37,7 @@ while [ $i -ge 0 ]
 do
 	echo "warning $i"
 	screen -S vintagestory_server -p 0 -X stuff "/announce server will reboot in "$i" seconds.^M"
-	i=$(( i - 1 ))
+	i = $(( i - 1 ))
 	sleep 1
 done
 announce "Restarting server now have a nice day nya~^M"
